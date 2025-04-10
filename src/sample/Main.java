801f2d01c41c9b18685ab6da40bfb71520abbaca
Piotr Ratuschny <ufpnp@student.kit.edu>
2025-04-10 21:28:17 +0200

    IntelliJ autocommit

---
+++ src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
